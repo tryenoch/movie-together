a03f5cd763e478322846dabbbd68cc3b91ad87f9
enoch012 <zzan6238@naver.com>
2023-06-29 11:50:28 +0900

    0629 / 프로젝트 생성

---
+++ src/main/java/com/bitc/full505_team2_project/Full505Team2ProjectApplication.java
@@ -0,0 +1,13 @@
+package com.bitc.full505_team2_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Full505Team2ProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Full505Team2ProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bitc/full505_team2_project/Full505Team2ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bitc.full505_team2_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Full505Team2ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
