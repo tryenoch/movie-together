288022363180d2e9f0d64b985e8e040a20e62f18
simplelag <ioia33@Naver.com>
2023-06-30 14:04:36 +0900

    메인화면에서 데이터 가져오기 성송 검색기능 생성중

--- src/main/java/com/bitc/full505_team2_project/dto/DailyMovieDTO.java
+++ src/main/java/com/bitc/full505_team2_project/dto/DailyMovieDTO.java
@@ -4,9 +4,27 @@ import lombok.Data;
 
 @Data
 public class DailyMovieDTO {
-    private String rank; // 일간 영화 랭킹
-    private String movieNm; // 영화 이름
-
-    private String openDt; // 영화 개봉일
-    private String audiAcc; // 누적 관객수
+//    private String rank; // 일간 영화 랭킹
+//    private String movieNm; // 영화 이름
+//
+//    private String openDt; // 영화 개봉일
+//    private String audiAcc; // 누적 관객수
+    private String rnum;
+    private String rank;
+    private String rankInten;
+    private String rankOldAndNew;
+    private String movieCd;
+    private String movieNm;
+    private String openDt;
+    private String salesAmt;
+    private String salesShare;
+    private String salesInten;
+    private String salesChange;
+    private String salesAcc;
+    private String audiCnt;
+    private String audiInten;
+    private String audiChange;
+    private String audiAcc;
+    private String scrnCnt;
+    private String showCnt;
 }
