603c699139dca7d9fc510a2c3c331afc4e452d84
gidulap <pol0258@naver.com>
2023-07-01 19:32:05 +0900

    병합

--- src/main/java/com/bitc/full505_team2_project/dto/MovieDto.java
+++ src/main/java/com/bitc/full505_team2_project/dto/MovieDto.java
@@ -1,14 +1,8 @@
 package com.bitc.full505_team2_project.dto;
 
 import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
 
 @Data
-@Getter
-@Setter
-public class MovieDto {
-    int moviePk;
-    String movieTitle;
-    int movieLikeCnt;
+public class MovieDTO {
+    private MovieResultDTO movieResultDTO;
 }
