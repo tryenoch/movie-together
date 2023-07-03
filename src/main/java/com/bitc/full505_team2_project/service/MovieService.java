fb76c0fe08918b00688d4546c7c69fd1b8896936
simplelag <ioia33@Naver.com>
2023-07-03 15:10:42 +0900

    검색 페이지 api 호출 성공 , card 사용하여  api로 불러온 데이터 표시

--- src/main/java/com/bitc/full505_team2_project/service/MovieService.java
+++ src/main/java/com/bitc/full505_team2_project/service/MovieService.java
@@ -1,9 +1,11 @@
 package com.bitc.full505_team2_project.service;
 
 import com.bitc.full505_team2_project.dto.DailyMovieDTO;
+import com.bitc.full505_team2_project.dto.MovieDetailDTO;
 
 import java.util.List;
 
 public interface MovieService {
     List<DailyMovieDTO> getDailyMovieList(String url) throws Exception;
+    List<MovieDetailDTO> getMovieResarchList(String url) throws Exception;
 }
